7fbd0f8a3f43ecd5afae39ed971c64cf8ceed1f5
김지섭 <0226daniel@gmail.com>
2018-06-11 11:53:52 +0900

    최신 테마 구조 적용

--- app/src/main/java/com/kimjisub/launchpad/theme/template/MainActivity.java
+++ app/src/main/java/com/kimjisub/launchpad/theme/template/MainActivity.java
@@ -1,10 +1,10 @@
 package com.kimjisub.launchpad.theme.template;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
